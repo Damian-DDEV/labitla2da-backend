84140dbacf58a417d7461121f024db772895286d
Damian-DDEV <damianmarquesa@gmail.com>
2022-08-30 10:27:53 -0300

    Validaciones del lado de sequelize, es decir para cada uno de los campos en el model

--- src/models/users.js
+++ src/models/users.js
@@ -16,26 +16,104 @@ module.exports = (sequelize, DataTypes) => {
   Users.init({
     username: {
       type: DataTypes.STRING,
-      unique: "The user name is already in use",
+      unique: "The username is already in use",
       validate: {
         notEmpty: {
-            msg: "El campo usuario no puede estar vacio."
+            msg: "The username field cannot be empty."
         },
         notNull: {
-            msg: "El campo usuario no puede ser nulo."
+            msg: "The username field cannot be null."
         },
         isAlpha: {
-            msg: "El campo usuario no puede tener caracteres especiales."
+            msg: "The username field cannot contain special characters."
         }
       }
     },
-    rol: DataTypes.INTEGER,
-    username: DataTypes.STRING,
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    dni: DataTypes.STRING
+    rol:  {
+      type: DataTypes.INTEGER,
+      validate: {
+        notEmpty: {
+            msg: "The rol field cannot be empty.."
+        },
+        isAlpha: {
+            msg: "The rol field cannot contain special characters."
+        }
+      }
+    },
+
+    firstname: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+            msg: "The firstname field cannot be empty."
+        },
+        notNull: {
+            msg: "The firstname field cannot be null."
+        },
+        isAlpha: {
+            msg: "The username field cannot contain special characters."
+        }
+      }
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+            msg: "The lastname field cannot be empty."
+        },
+        notNull: {
+            msg: "The lastname field cannot be null."
+        },
+        isAlpha: {
+            msg: "The username field cannot contain special characters."
+        }
+      }
+    },
+    password:  {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+            msg: "The lastname field cannot be empty."
+        },
+        notNull: {
+            msg: "The lastname field cannot be null."
+        },
+        isAlpha: {
+            msg: "The username field cannot contain special characters."
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: "The email is already in use",
+      validate: {
+        isEmail: true,
+        notEmpty: {
+            msg: "The email field cannot be empty."
+        },
+        notNull: {
+            msg: "The email field cannot be null."
+        },
+        isAlpha: {
+            msg: "The username field cannot contain special characters."
+        }
+      }
+    },
+    dni:  {
+      type: DataTypes.STRING,
+      unique: "The dni/id is already in use",
+      validate: {
+        notEmpty: {
+            msg: "The dni/id field cannot be empty."
+        },
+        notNull: {
+            msg: "The dni/id field cannot be null."
+        },
+        isAlpha: {
+            msg: "The dni/id field cannot contain special characters"
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'Users',
